d30167d20d0ec2e72a4ef28b6f30042850a40746
Mark <mark@remarkablemark.org>
2020-12-19 17:50:30 -0500

    chore: set up TypeScript project
    Based on https://github.com/remarkablemark/phonetic-alphabet-converter

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+
+/**
+ * Build rollup config for development (default) or production (minify = true).
+ *
+ * @param {Boolean} [minify=false]
+ * @return {Object}
+ */
+const config = (minify = false) => ({
+  input: 'src/index.ts',
+  output: {
+    file: `umd/mdtocs${minify ? '.min' : ''}.js`,
+    format: 'umd',
+    name: 'mdtocs',
+    sourcemap: true,
+  },
+  plugins: [typescript({ module: 'es2015' }), minify && terser()],
+});
+
+export default [config(), config(true)];
