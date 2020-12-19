26f9ecf281db23ea6a81aeceb0334f294a55e8c4
Mark <mark@remarkablemark.org>
2020-12-19 18:13:05 -0500

    feat(index): add and export `mdtocs`
    https://jsfiddle.net/remarkablemark/o0mja3hf/9/

---
+++ src/index.ts
@@ -0,0 +1,104 @@
+const HEADINGS_REGEX = /^(#{1,6}[ \t].+)$|^(.+[\r\n][=-]{3,})$/gm;
+const HEADING_REGEX = /^(#+)[ \t](.+)$|^(.+)[\r\n]([=-])/;
+const WHITESPACE_REGEX = /\s/;
+const INVALID_FRAGMENT_REGEX = /[^a-zA-Z0-9_-]/g;
+
+const SPACE = ' ';
+const HYPHEN = '-';
+const MARKDOWN_BULLET = HYPHEN + SPACE;
+const MARKDOWN_INDENT_LENGTH = 2;
+const MARKDOWN_INDENT = Array(MARKDOWN_INDENT_LENGTH + 1).join(SPACE); // SPACE + SPACE
+const NEWLINE = '\n';
+
+type FragmentCountMap = Record<string, number>;
+
+/**
+ * Transforms heading to URL fragment (without #).
+ */
+function getFragment(heading: string): string {
+  return heading
+    .toLowerCase()
+    .split(WHITESPACE_REGEX)
+    .join('-')
+    .replace(INVALID_FRAGMENT_REGEX, '');
+}
+
+/**
+ * Transforms heading and fragment to Markdown.
+ */
+function getLink(heading: string, fragment: string): string {
+  return '[' + heading + '](#' + fragment + ')';
+}
+
+/**
+ * Generates table of contents given Markdown.
+ */
+export function mdtocs(markdown: string): string {
+  if (typeof markdown !== 'string') {
+    throw new TypeError('First argument must be a string');
+  }
+
+  const headings = markdown.match(HEADINGS_REGEX);
+
+  if (headings === null) {
+    return '';
+  }
+
+  let result = '';
+  let previousLevel = null;
+  let indent = '';
+  const fragmentCountMap: FragmentCountMap = {};
+
+  for (let i = 0, len = headings.length; i < len; i++) {
+    const heading = headings[i].match(HEADING_REGEX);
+
+    if (heading === null) {
+      continue;
+    }
+
+    let currentLevel;
+    let headingText;
+
+    // get heading level and text
+    if (heading[4]) {
+      headingText = heading[3].trim();
+      if (!headingText) {
+        continue;
+      }
+      currentLevel = heading[4] === HYPHEN ? 2 : 1;
+    } else {
+      headingText = heading[2].trim();
+      if (!headingText) {
+        continue;
+      }
+      currentLevel = heading[1].length;
+    }
+
+    // build indent
+    if (!previousLevel) {
+      // continue
+    } else if (currentLevel === 1) {
+      indent = '';
+    } else if (currentLevel > previousLevel) {
+      indent += MARKDOWN_INDENT;
+    } else if (currentLevel < previousLevel) {
+      indent = indent.slice(MARKDOWN_INDENT_LENGTH);
+    }
+    previousLevel = currentLevel;
+
+    let fragment = getFragment(headingText);
+    const fragmentCount = fragmentCountMap[fragment];
+
+    if (fragmentCount > 0) {
+      fragmentCountMap[fragment] += 1;
+      fragment += '-' + fragmentCount;
+    } else if (fragmentCount === undefined) {
+      fragmentCountMap[fragment] = 1;
+    }
+
+    result +=
+      indent + MARKDOWN_BULLET + getLink(headingText, fragment) + NEWLINE;
+  }
+
+  return result;
+}
